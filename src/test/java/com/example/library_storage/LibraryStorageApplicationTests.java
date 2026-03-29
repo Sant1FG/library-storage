498538146c705657cbacfd384ecbd910243e230f
Sant1FG <92580267+Sant1FG@users.noreply.github.com>
2026-03-29 17:02:26 +0200

    Initial commit

---
+++ src/test/java/com/example/library_storage/LibraryStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
